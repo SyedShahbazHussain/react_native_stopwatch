8502ddae8a563f8a10693fc283c9d4630c3ba3ab
SyedShahbazHussain <syedshahbaz.hussain@outlook.com>
2019-05-01 23:55:59 +0500

    Starting

---
+++ App.js
@@ -0,0 +1,33 @@
+import React,{ Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const DATA ={
+  timer: 1234567,
+  laps: [12345, 2345, 34567, 98765],
+}
+
+function Timer ({interval}){
+  return <Text style={styles.timer}>{interval}</Text>
+}
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      <Timer interval={DATA.timer}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '0D0D0D',
+    alignItems: 'center',
+  },
+  timer:{
+    color: '#ffffff',
+    fontSize: 76,
+    fontWeight: '200',
+  }
+});
